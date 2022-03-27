4ce8d396d81921ef285e595ca278a14994625f69
harsh <harshagarwal26052001@gmail.com>
2022-03-27 09:19:09 +0530

    added all the components in the git repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Navbar from "./components/common/Navbar";
+// -
+import About from "./components/common/About";
+import ProductPage from './components/common/ProductPage';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile.js';
+import Seller from './pages/Seller.js';
+import {
+  BrowserRouter as Router,
+  // Switch,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/products/:category" element={<ProductPage />}></Route>
+          <Route exact path="/auth/login" element={<Login />}></Route>
+          <Route exact path="/auth/signup" element={<SignUp />}></Route>
+          <Route exact path="/profile" element={<Profile />}></Route>
+          <Route exact path="/addproduct" element={<Seller />}></Route>
+        </Routes>
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/components/common/ProductPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductPage = () => {
+    return (
+        <div>
+            productpages
+        </div>
+    )
+}
+
+export default ProductPage
